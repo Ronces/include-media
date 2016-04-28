9ede5ff746d76cae84d4fe5e0fb5c32d0092b6b6
Ronces <matthieu.eschbach@gmail.com>
2016-04-28 13:32:51 +0200

    checking for an existant /lib/include-media while installing and not show an error

--- install.sh
+++ install.sh
@@ -2,14 +2,17 @@
 
 echo "copying binary files..."
 sudo cp bin/* /bin/
+
 echo "copying librarys..."
+if ! [ -d "/lib/include-media" ]; then
 sudo mkdir /lib/include-media
+fi
 sudo cp lib/* /lib/include-media
 
 echo "copying config file..."
 if [ -f "/etc/include-media.cfg" ]; then
   echo "Config file allready exists, nothing to do."
 else
-  cp sudo etc/* /etc/include-media
+  sudo cp etc/* /etc/
   echo "please make sure to edit /etc/include-media.cfg with your own paths"
 fi
