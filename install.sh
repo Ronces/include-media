ed9f4bc46ff2d31e71fd979786963b02b618da6b
Ronces <matthieu.eschbach@gmail.com>
2016-04-28 11:41:26 +0200

    making a better install

--- install.sh
+++ install.sh
@@ -3,7 +3,14 @@
 echo "copying binary files..."
 sudo cp bin/* /bin/
 echo "copying librarys..."
-sudo cp lib/* /lib/
+sudo mkdir /lib/include-media
+sudo cp lib/* /lib/include-media
+
 echo "copying config files..."
-sudo cp etc/* /etc/
-echo "please make sure to edit /etc/include-media.cfg with your own paths"
+if [ -d "$HOME/.include-media" ]; then
+  echo "Config files allready exists, nothing to do."
+else
+  mkdir $HOME/.include-media
+  cp etc/* $HOME/.include-media
+  echo "please make sure to edit /etc/include-media.cfg with your own paths"
+fi
